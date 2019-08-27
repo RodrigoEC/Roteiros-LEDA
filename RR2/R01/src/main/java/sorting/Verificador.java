1455cb9f67083feed543e0d066802a5d0a3da3c2
rodrigoec <rodrigo.cavalcanti@ccc.ufcg.edu.br>
2019-08-26 22:20:30 -0300

    Its the end of the journey

---
+++ RR2/R01/src/main/java/sorting/Verificador.java
@@ -0,0 +1,13 @@
+package sorting;
+
+public class Verificador<T extends Comparable<T>> {
+    public boolean verificador(T[] array, int leftInder, int rightIndex) {
+        if (rightIndex <= leftInder) {
+            return false;
+        } else if (array.length != 0 && leftInder >= 0 && rightIndex <= array.length) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
