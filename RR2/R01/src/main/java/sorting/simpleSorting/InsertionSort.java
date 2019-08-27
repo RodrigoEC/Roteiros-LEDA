6bad0db499cdc5b758d2e186db37c04273eefd87
rodrigoec <rodrigo.cavalcanti@ccc.ufcg.edu.br>
2019-08-26 22:48:15 -0300

    Its the end of the journey -- i guess

--- RR2/R01/src/main/java/sorting/simpleSorting/InsertionSort.java
+++ RR2/R01/src/main/java/sorting/simpleSorting/InsertionSort.java
@@ -1,6 +1,7 @@
 package sorting.simpleSorting;
 
 import sorting.AbstractSorting;
+import sorting.Verificador;
 
 import static util.Util.swap;
 
@@ -12,16 +13,22 @@ import static util.Util.swap;
  */
 public class InsertionSort<T extends Comparable<T>> extends AbstractSorting<T> {
 
+	Verificador verificar = new Verificador();
+
 	@Override
 	public void sort(T[] array, int leftIndex, int rightIndex) {
-		for (int i = leftIndex + 1; i < rightIndex; i++) {
-			T key = array[i];
-			int j = i - 1;
 
-			while(j >= 0 && key.compareTo(array[j]) < 0) {
-				swap(array, j, j + 1);
-				j--;
+		if (verificar.verificador(array, leftIndex, rightIndex)) {
+			for (int i = leftIndex + 1; i < rightIndex; i++) {
+				T key = array[i];
+				int j = i - 1;
+
+				while(j >= 0 && key.compareTo(array[j]) < 0) {
+					swap(array, j, j + 1);
+					j--;
+				}
 			}
+
 		}
 	}
 }
